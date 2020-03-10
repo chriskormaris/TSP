cb2ce9fd84e9103dc5b94dd4ca430b24cee17ed7
Christos Kormaris <chriskormaris@gmail.com>
2020-03-10 13:44:40 +0200

    Update runMainFixedCoordinates.bat
    Minor changes. Remove unneeded "\", "." and ";" characters.

--- runMainFixedCoordinates.bat
+++ runMainFixedCoordinates.bat
@@ -1,4 +1,4 @@
 :: This file is for Windows only.
 
-java -cp .\bin;.\lib\graph.jar;.\lib\gral-core-0.11.jar tsp_plot.MainFixedCoordinates
-pause;
+java -cp bin;lib\graph.jar;lib\gral-core-0.11.jar tsp_plot.MainFixedCoordinates
+pause
