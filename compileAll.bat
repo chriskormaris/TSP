ce2809833708f368d52b545ed094b4841fe076ec
Christos Kormaris <chriskormaris@gmail.com>
2020-03-10 13:43:59 +0200

    Update compileAll.bat
    Minor changes. Replace "mkdir bin" with "if not exist bin mkdir bin". Remove unneeded "\", "." and ";" characters.

--- compileAll.bat
+++ compileAll.bat
@@ -1,5 +1,5 @@
 :: This file is for Windows only.
 
-mkdir .\bin
-javac .\src\tsp\*.java .\src\tsp_plot\*.java -d .\bin\ -cp .\lib\graph.jar;.\lib\gral-core-0.11.jar;
-pause;
+if not exist bin mkdir bin
+javac src\tsp\*.java src\tsp_plot\*.java -d bin -cp lib\graph.jar;lib\gral-core-0.11.jar;
+pause
